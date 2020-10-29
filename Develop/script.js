f7591668fbacaeb64607071c2b88a708f89ed6cc
Melody Monarch <oliva.sam.so@gmail.com>
2020-10-28 21:25:22 -0500

    comments, functions, variables, while loops, & conditionals added

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,60 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+ var password = generatePassword();
+ var passwordText = document.querySelector("#password");
+}
 
-  passwordText.value = password;
+function generatePassword() {
+// Pop-Up for Character Length
+var characterLength = 0
+while(characterLength < 8 || characterLength > 128){
+characterLength = parseInt(prompt("How many characters would you like your password to contain? \n (must be between 8 - 128 characters)"))
+}
+var specialCharacters = false
+var numericCharacters = false
+var lowercaseCharacters = false
+var uppercaseCharacters = false
+while(!specialCharacters && !numericCharacters && !uppercaseCharacters && !lowercaseCharacters){
 
+  // Pop-Up for Special Characters
+  specialCharacters = confirm("Click OK to include Special characters \n  !#$%&'()*+,-./:;<=>?@[]^_`{|}~")
+  
+  // Pop-Up for Numeric Characters
+  numericCharacters = confirm("Click OK to include Numeric characters \n 0123456789")
+  
+  // Pop-Up for Lowercase Characters
+  lowercaseCharacters = confirm("Click OK to include lowercase characters")
+  
+  // Pop-Up for Uppercase Characters
+  uppercaseCharacters = confirm("Click OK to include UPPERCASE characters")
+  
+}
+
+var userCharacters = []
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "0123456789"
+var specials = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+if(specialCharacters){
+  userCharacters +=specials;
+}
+
+if(numericCharacters){
+  userCharacters +=numbers;
+}
+
+if(lowercaseCharacters){
+  userCharacters +=lowercaseLetters;
+}
+
+if(uppercaseCharacters){
+  userCharacters +=uppercaseLetters;
+}
+
+console.log(userCharacters, characterLength)
+  //passwordText.value = password;
 }
 
 // Add event listener to generate button
